544618a3501eed521124b13ab70d90cfa099ed3f
RoyMust <27915388.silva@gmail.com>
2022-12-02 13:14:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+import Header from './Components/Header';
+import Post, { PostProps } from './Components/Post';
+import Profile from './Components/Profile';
+
+interface Post extends PostProps{
+  id:number;
+}
+
+const posts:Post[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/Fernanda.png",
+      name: "Fernanda",
+      role: "Web Developer"
+    },
+    content: [
+    {type: 'paragraph', content:'Olá Pessoal, meu nome é Fernanda e eu sou o primeiro a postar algo aqui no Iginte Feed'},
+    {type: 'paragraph', content:'To fazendo um curso louco'},
+    {type: 'link', content:'fernandagomes@gmail.com'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/Jonas.png",
+      name: "Israel Mendes",
+      role: "Designer"
+    },
+    content: [
+      {type: 'paragraph', content:'To fazendo um curso louco'},
+      {type: 'paragraph', content:'Olá Pessoal, meu nome é Fernanda e eu sou o primeiro a postar algo aqui no Iginte Feed'},
+      {type: 'link', content:'fernandogomes@gmail.com'},
+      ],
+  publishedAt: new Date('2022-08-16 22:00:00'),
+},
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/Gabriele.png",
+      name: "jonas",
+      role: "C.O"
+    },
+    publishedAt: new Date('2022-08-16 22:00:00'),
+    content: [
+      {type: 'paragraph', content:'Olá Pessoal, meu nome é lopesFernanda e eu sou o primeiro a postar algo aqui no Iginte Feed'},
+      {type: 'paragraph', content:'To fazendo um curso louco mané'},
+      {type: 'link', content:'lopesgomes@gmail.com'},
+      ],
+  }
+];
+
+
+export default function App() {
+  return (
+    <Container>
+      <Header />
+      <Wrapper>
+        <Profile />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </Wrapper>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+`
+const Wrapper = styled.div`
+margin: 1rem auto;
+padding: 0 1rem;
+
+max-width: 60rem;
+grid-template-columns: 256px 1fr;
+gap: 0.8rem;
+display: grid;
+
+align-items: flex-start;
+
+@media (max-width: 768px){
+  grid-template-columns: 1fr;
+}
+`
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,95 @@
+import styled from 'styled-components'
+
+export default function Profile() {
+  return (
+    <Container>
+      <PositionRelative>
+        <img src='https://github.com/RoyMust.png' />
+      </PositionRelative>
+      <Header>
+        <img src="https://cdn.pixabay.com/photo/2016/11/19/15/32/laptop-1839876_960_720.jpg"/>
+      </Header>
+      <TextProfile>
+        <strong>Samuel Mendes</strong>
+        <span>Web Developer</span>
+      </TextProfile>
+      <Footer>
+        <button>
+          Editar seu perfil
+        </button>
+      </Footer>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  border: 1px solid black;
+  overflow: hidden;
+  border-radius: 8px;
+  position: relative;
+  color: white;
+  box-shadow: rgb(52, 60, 69) 0px 20px 30px -10px;  
+  height: 20rem;
+
+`
+
+const TextProfile = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin: 1rem 0px 1rem;
+  
+
+strong{
+  text-transform: uppercase;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+span{
+  margin-top: 5px;
+  font-family: 'Courier New', Courier, monospace;
+}
+`
+const Footer = styled.div`
+  display: flex;
+  justify-content: center;
+
+button{
+  background-color: transparent;
+  color: white;
+  font-family:  'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-size: 1rem;
+  width: 10rem;
+  height: 3rem;
+  margin: 1rem;
+  border-radius: 8px;
+  border: 2px solid green;
+  cursor: pointer;
+
+  &:hover{
+    background-color: green;
+    transition: 0.1s
+  }
+}
+`
+const Header = styled.div`
+  height: 9rem;
+  img{
+      height: 6rem;
+      width: 100%;
+    }
+`
+const PositionRelative = styled.div`
+  position: absolute;
+  top: 3.5rem;
+  left: 50%;
+  transform: translateX(-50%);
+
+  img{
+  background-color: #2c2c2c;
+  padding: 3px;
+  height: 5rem;
+  width: 5rem;
+  border-radius: 8px;
+  border: 2px solid green;
+  }
+`
